b9cb1a2924cd1725f83a8f7185f457389bd04598
Vincent Pun <vincent@ufinance.co>
2018-05-06 18:03:04 +0800

    2 Add view parameters

--- public/app.js
+++ public/app.js
@@ -2,17 +2,19 @@
 
 var learnjs = {};
 
-learnjs.problemView = function() {
-  return $('<div class="problem-view">').text('Coming Soon!');
+learnjs.problemView = function(problemNumber) {
+  var title = 'Problem #' + problemNumber + ' Coming soon!';
+  return $('<div class="problem-view">').text(title);
 }
 
 learnjs.showView = function(hash) {
   var routes = {
-    '#problem-1': learnjs.problemView,
+    '#problem': learnjs.problemView,
   };
-  var viewFn = routes[hash];
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
 
   if (viewFn) {
-    $('.view-container').empty().append(viewFn());
+    $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 };
