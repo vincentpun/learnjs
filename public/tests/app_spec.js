f216fa9c5feab4285d37e8090f6cf5b2668140c4
Vincent Pun <vincent@ufinance.co>
2018-05-12 20:16:34 +0800

    3.4 Flash result

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -47,16 +47,23 @@ describe('LearnJS', function() {
     });
 
     describe('answer section', function() {
+      var resultFlash;
+
+      beforeEach(function() {
+        spyOn(learnjs, 'flashElement');
+        resultFlash = view.find('.result');
+      });
+
       it('can check a correct answer by hitting a button', function() {
         view.find('.answer').val('true');
         view.find('.check-btn').click();
-        expect(view.find('.result').text()).toEqual('Correct!');
+        expect(learnjs.flashElement).toHaveBeenCalledWith(resultFlash, 'Correct!');
       });
 
       it('rejects an incorrect answer', function() {
         view.find('.answer').val('false');
         view.find('.check-btn').click();
-        expect(view.find('.result').text()).toEqual('Incorrect!');
+        expect(learnjs.flashElement).toHaveBeenCalledWith(resultFlash, 'Incorrect!');
       });
     });
   });
